69d437ec324e9eb61d3391352b411a2f4ba24cad
Atharva Ashish Chitnavis <47708712+ath98@users.noreply.github.com>
2020-09-10 21:09:31 +0530

    New start

--- registration/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.register_student, name = 'studentRegistration'),
-    path('register', views.register, name = 'register'),
-]
