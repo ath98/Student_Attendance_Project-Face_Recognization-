4657971d3a54939923d5dfe8b31f84ff0ad601f4
Atharva Ashish Chitnavis <47708712+ath98@users.noreply.github.com>
2020-09-28 12:55:24 +0530

    merging Ath

--- attendance/reports.py
+++ attendance/reports.py
@@ -6,19 +6,27 @@ def byLecture(request): #GET REPORTS OF WHOLE LECTURE
     if request.method == 'POST':
         lecId = request.POST.get("ID")
         obj = Attendance.objects.get(lecture_number= lecId)
-        return render(request,'templates/reports.html', obj)      
+        context{
+            'obj':obj
+        }
+        return render(request,'templates/reports.html', context)      
 
 def byDefaulter(request): #GET REPORTS OF A SPECIFIC STUDENT AND SUBJECT
     if request.method == 'POST':
         roll = request.POST.get("roll")
         shift = request.POST.get("shift")
-        obj = Attendance.objects.filter(rollnumber = roll, shift=shift)        
-        return render(request,'templates/reports.html', obj)
+        obj = Attendance.objects.filter(rollnumber = roll, shift=shift)   
+        context{
+            'obj':obj
+        }     
+        return render(request,'templates/reports.html', context)
 
 def reportsByRoll(request): #GET REPORTS BY ROLL NUMBER FOR ALL SUBJECTS
     if request.method == 'POST':
         roll = request.POST.get("roll")
         obj = Attendance.objects.get(rollnumber = roll)
-        return render(request,'templates/reports.html', obj)
+        context{
+            'obj':obj
+        }
+        return render(request,'templates/reports.html', context)
 
-        
