33ab44a10873eb85e69c4ed8c0f9f45e180c13dc
Atharva Ashish Chitnavis <47708712+ath98@users.noreply.github.com>
2020-10-07 13:43:57 +0530

    working on face detection and student reg

--- attendance/face_detection.py
+++ attendance/face_detection.py
@@ -86,69 +86,52 @@ def saveData(details):
     return True
 
 
-def takeAttendance():
+# # def attendance():
 
-    # Access details ( year, shift ) to access folder from base directory
-    # use those details to append dir path to IMG_ROOT
+# #     # Access details ( year, shift ) to access folder from base directory
+# #     # use those details to append dir path to IMG_ROOT
 
-    datasets = os.path.join(IMG_ROOT)
-    # Creating a cascade image classifier
-    faceDetect = cv2.CascadeClassifier(
-        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+# #     datasets = "C:/Users/Lenovo/Documents/GitHub/Student_Attendance_Project-Face_Recognization-/datasets"
+# #     # Creating a cascade image classifier
+# #     faceDetect = cv2.CascadeClassifier(
+# #         cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-    # Traverse through the dataset
-    (images, lables, names, id) = ([], [], {}, 0)
-    for (subdirs, dirs, files) in os.walk(datasets):
-        for subdir in dirs:
-            names[id] = subdir
-            subjectpath = os.path.join(datasets, subdir)
-            for filename in os.listdir(subjectpath):
-                path = subjectpath + '/' + filename
-                lable = id
-                images.append(cv2.imread(path, 0))
-                lables.append(int(lable))
-            id += 1
-            print(path)
-    # train model to recognize faces
-    (images, lables) = [numpy.array(lis) for lis in [images, lables]]
-    model = cv2.face.LBPHFaceRecognizer_create()
-    model.train(images, lables)
-    # camture images from the webcam and process and detect the face
-    # takes video capture id, for webcam most of the time its 0.
-    cam = cv2.VideoCapture(0)
-    # Our identifier
-    # We will put the id here and we will store the id with a face, so that later we can identify whose face it is
-    # Our dataset naming counter
-    sampleNum = 0
-    # Capturing the faces one by one and detect the faces and showing it on the window
-    while(True):
-        # Capturing the image
-        # cam.read will return the status variable and the captured colored image
-        ret, img = cam.read()
-        # the returned img is a colored image but for the classifier to work we need a greyscale image
-        # to convert
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # To store the faces
-        # This will detect all the images in the current frame, and it will return the coordinates of the faces
-        # Takes in image and some other parameter for accurate result
-        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-        # In above 'faces' variable there can be multiple faces so we have to get each and every face and draw a rectangle around it.
-        for(x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            face = gray[y:y + h, x:x + w]
-            face_resize = cv2.resize(face, (80, 130))
-            # Recognizing face here
-            prediction = model.predict(face_resize)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+# #     # Traverse through the dataset
+# #     (images, lables, names, id) = ([], [], {}, 0) 
+# #     for (subdirs, dirs, files) in os.walk(datasets): 
+# #         for subdir in dirs: 
+# #             names[id] = subdir 
+# #             subjectpath = os.path.join(datasets, subdir) 
+# #             for filename in os.listdir(subjectpath): 
+# #                 path = subjectpath + '/' + filename 
+# #                 lable = id
+# #                 images.append(cv2.imread(path, 0)) 
+# #                 lables.append(int(lable)) 
+# #             id += 1
+# #     (images, lables) = [numpy.array(lis) for lis in [images, lables]]
+# #     model = cv2.face.LBPHFaceRecognizer_create()
+# #     model.train(images, lables)
+# #     cam = cv2.VideoCapture(0)
+# #     sampleNum = 0
+# #     while(True):
+# #         ret, img = cam.read()
+# #         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# #         faces = faceDetect.detectMultiScale(gray, 1.3, 5)        
+# #         for(x, y, w, h) in faces:
+# #             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+# #             face = gray[y:y + h, x:x + w]
+# #             face_resize = cv2.resize(face, (80, 130))
+#             prediction = model.predict(face_resize)
+#             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-            if prediction[1] < 500:
-                cv2.putText(img, '% s - %.0f' % (names[prediction[0]], prediction[1]),
-                            (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-            else:
-                cv2.putText(img, 'not recognized', (x-10, y-10),
-                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
-        cv2.imshow('OpenCV', img)
-        key = cv2.waitKey(10)
-        if key == 27:
-            break
-    cv2.destroyAllWindows()
+#             if prediction[1] < 500:
+#                 cv2.putText(img, '% s - %.0f' % (names[prediction[0]], prediction[1]),
+#                             (x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+#             else:
+#                 cv2.putText(img, 'not recognized', (x-10, y-10),
+#                             cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+#         cv2.imshow('OpenCV', img)
+#         key = cv2.waitKey(10)
+#         if key == 27:
+#             break
+#     cv2.destroyAllWindows()
--- attendance/views.py
+++ attendance/views.py
@@ -9,16 +9,16 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.utils import tree
 from django.views.generic import FormView  
-from .face_detection import takeAttendance
 
 import json
+import numpy as np
 
 from .models import Faculty, Student,Subject,Lecture
 from .forms import FacultyChoiceField
 
 from .face_detection import saveData
 
-from fd.settings import BASE_DIR
+from fd.settings import BASE_DIR, IMG_ROOT
 
 import os
 import cv2
@@ -276,96 +276,78 @@ def take_attendance(request):    #get values from the fields lectureid ,subject,
 
 @login_required(login_url='login')
 def takeAttendance(request):
+    
+    faceDetect = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    dataset = IMG_ROOT
+    (images, lables, names, id) = ([], [], {}, 0) 
+    for (subdirs, dirs, files) in os.walk(dataset): 
+        for subdir in dirs: 
+            names[id] = subdir 
+            subjectpath = os.path.join(dataset, subdir) 
+            #print(subjectpath)
+            for filename in os.listdir(subjectpath): 
+                path = subjectpath + '/' + filename 
+                lable = id
+                images.append(cv2.imread(path, 0)) 
+                lables.append(int(lable)) 
+            id += 1  
+            print(filename)
+    print(path)
+    (images, lables) = [np.array(lis) for lis in [images, lables]]
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.train(images, lables)    
+     
+    #path = BASE_DIR.joinpath('dataset')    
+    
+    cam = cv2.VideoCapture(0)
+    sampleNum = 0    
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(
+            gray,
+            scaleFactor=1.3,
+            minNeighbors=3,
+            minSize=(30, 30)
+        )
 
-    # print(cv2.__version__)
-    # # Detect face
-    # # Creating a cascade image classifier
-    # faceDetect = cv2.CascadeClassifier(
-    #     cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    # print(faceDetect)
+        for (x, y, w, h) in faces:
+            face = gray[y:y + h, x:x + w]
+            face_resize = cv2.resize(face, (80, 130))
+            prediction = model.predict(face_resize)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # dataset = os.path.join(BASE_DIR, 'datasets', 'test')
-    # print(dataset)
-    # path = 'G:/project/fd/attendance/0.jpg'
-    # # im = Image.open("G:/project/fd/attendance/0.jpg")
-    # # im.show()
-    # print(BASE_DIR)
+        cv2.imshow("Face", img)
+        
+        key = cv2.waitKey(10)
+        if key == 27:
+            break
+    # cv2.destroyAllWindows()
 
-    # path = BASE_DIR.joinpath('dataset')
-    # print(path)
-
-    # # image = cv2.imread(path)
-    # # while(True):
-    # #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # #     faces = faceCascade.detectMultiScale(
-    # #         gray,
-    # #         scaleFactor=1.3,
-    # #         minNeighbors=3,
-    # #         minSize=(30, 30)
-    # #     )
-
-    # #     for (x, y, w, h) in faces:
-    # #         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # #     cv2.imshow("Face", image)
-
-    # # camture images from the webcam and process and detect the face
-    # # takes video capture id, for webcam most of the time its 0.
     # cam = cv2.VideoCapture(0)
 
-    # # Our identifier
-    # # We will put the id here and we will store the id with a face, so that later we can identify whose face it is
-    # # Our dataset naming counter
     # sampleNum = 0
-    # # Capturing the faces one by one and detect the faces and showing it on the window
     # while(True):
-    #     # Capturing the image
-    #     # cam.read will return the status variable and the captured colored image
     #     ret, img = cam.read()
-    #     # the returned img is a colored image but for the classifier to work we need a greyscale image
-    #     # to convert
     #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #     # To store the faces
-    #     # This will detect all the images in the current frame, and it will return the coordinates of the faces
-    #     # Takes in image and some other parameter for accurate result
     #     faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-    #     # In above 'faces' variable there can be multiple faces so we have to get each and every face and draw a rectangle around it.
     #     for(x, y, w, h) in faces:
-    #         # Whenever the program captures the face, we will write that is a folder
-    #         # Before capturing the face, we need to tell the script whose face it is
-    #         # For that we will need an identifier, here we call it id
-    #         # So now we captured a face, we need to write it in a file
     #         sampleNum = sampleNum+1
-    #         # Saving the image dataset, but only the face part, cropping the rest
     #         cv2.imwrite(path + str(sampleNum)+'.jpg', gray[y:y+h, x:x+w])
-    #         # @params the initial point of the rectangle will be x,y and
-    #         # @params end point will be x+width and y+height
-    #         # @params along with color of the rectangle
-    #         # @params thickness of the rectangle
     #         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    #         # Before continuing to the next loop, I want to give it a little pause
-    #         # waitKey of 100 millisecond
     #         cv2.waitKey(250)
 
-    #     # Showing the image in another window
-    #     # Creates a window with window name "Face" and with the image img
     #     cv2.imshow("Face", img)
-    #     # Before closing it we need to give a wait command, otherwise the open cv wont work
-    #     # @params with the millisecond of delay 1
     #     cv2.waitKey(1)
-    #     # To get out of the loop
     #     if(sampleNum > 50):
     #         break
-    # # releasing the cam
     # cam.release()
-    # # destroying all the windows
     # cv2.destroyAllWindows()
-    takeAttendance()
     context = {}
     return render(request, 'templates/login.html', context)
 
 def reports(request):  
     context = {}
-    render(request,'templates/reports.html',context)
+    return render(request,'templates/reports.html',context)
         
